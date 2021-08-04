04f6c53d08ea9d064ecc2ccb557ee44116e001a2
pvictor <victor.perrier@dreamRs.fr>
2021-08-04 14:31:22 +0200

    added argument ignore_doc_type to check_Rd_*()

--- R/check_Rd.R
+++ R/check_Rd.R
@@ -1,7 +1,11 @@
 
-#' Check Rd file
+#' @title Check Rd file
+#' 
+#' @description Check documentation files to be sure each function have an example and a return value.
 #'
 #' @param path Path to Rd files, typically \code{man/}
+#' @param ignore_doc_type Ignore specific documentation type.
+#'  Default is to ignore documentation for datasets and package. Use \code{NULL} to cjeck all files.
 #'
 #' @return Character vector of Rd files path.
 #' @export
@@ -18,25 +22,29 @@
 #' check_Rd_value()
 #' 
 #' }
-check_Rd_examples <- function(path = "man/") {
-  check_Rd_tag(path = path, tag = "\\examples{")
+check_Rd_examples <- function(path = "man/", ignore_doc_type = c("data", "package")) {
+  check_Rd_tag(path = path, tag = "\\examples{", ignore_doc_type = ignore_doc_type)
 }
 
 #' @export
 #' 
 #' @rdname check-rd
-check_Rd_value <- function(path = "man/") {
-  check_Rd_tag(path = path, tag = "\\value{")
+check_Rd_value <- function(path = "man/", ignore_doc_type = c("data", "package")) {
+  check_Rd_tag(path = path, tag = "\\value{", ignore_doc_type = ignore_doc_type)
 }
 
 
-check_Rd_tag <- function(path = "man/", tag = "\\examples{") {
+check_Rd_tag <- function(path = "man/", tag = "\\examples{", ignore_doc_type = c("data", "package")) {
   tag_ <- gsub(pattern = "[^[:alpha:]]", replacement = "", x = tag)
+  if (!is.null(ignore_doc_type))
+    ignore_doc_type <- sprintf("\\docType{%s}", ignore_doc_type)
   Rd <- list.files(path = path, pattern = "Rd$")
   has_tag <- vapply(
     X = Rd,
     FUN = function(file) {
       doc <- readLines(file.path(path, file))
+      if (!is.null(ignore_doc_type) && any(ignore_doc_type %in% doc))
+        return(TRUE)
       tag %in% doc
     },
     FUN.VALUE = logical(1)
